41d8a838a1c552da3d6f65fe1e794b4678b719b7
Juan P. Dominguez-Morales <jpdominguez@atc.us.es>
2016-01-23 13:30:17 +0100

    Added hyperlinks to the About window and removed ClassDiagrams

--- NAVIS/NAVIS_VisualStudioProject/NAVIS/Screens/About.xaml.cs
+++ NAVIS/NAVIS_VisualStudioProject/NAVIS/Screens/About.xaml.cs
@@ -22,6 +22,7 @@
 
 using System;
 using System.Collections.Generic;
+using System.Diagnostics;
 using System.Linq;
 using System.Reflection;
 using System.Text;
@@ -33,6 +34,7 @@ using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
 using System.Windows.Shapes;
 
 namespace NAVIS
@@ -46,8 +48,91 @@ namespace NAVIS
         {
             InitializeComponent();
 
+            //Get NAVIS version
             System.Version version = Assembly.GetExecutingAssembly().GetName().Version;
-            textInfo.Text = "https://github.com/jpdominguez/NAVIS-Tool\nCopyright © 2015 Juan P. Dominguez-Morales\njpdominguez@atc.us.es\n\nRobotics and Techonology of Computers Lab. (RTC)\nwww.rtc.us.es\n\nDepartment of Architecture and Technology of Computers (ATC)\nwww.atc.us.es\n\nHigher Technical School of Computer Engineering\nUniversity of Seville, Spain\nwww.informatica.us.es";
+
+            #region textblock hyperlinks and text
+
+            //Creating texts
+            Run run1 = new Run("https://github.com/jpdominguez/NAVIS-Tool");
+            Run run2 = new Run("Copyright © 2015 Juan P. Dominguez-Morales");
+            Run run3 = new Run("jpdominguez@atc.us.es");
+            Run run4 = new Run("Robotics and Techonology of Computers Lab. (RTC)");
+            Run run5 = new Run("www.rtc.us.es");
+            Run run6 = new Run("Department of Architecture and Technology of Computers (ATC)");
+            Run run7 = new Run("www.atc.us.es");
+            Run run8 = new Run("Higher Technical School of Computer Engineering");
+            Run run9 = new Run("University of Seville, Spain");
+            Run run10 = new Run("www.informatica.us.es");
+
+            //Associating hyperlinks with text
+            Hyperlink hyperlink1 = new Hyperlink(run1)
+            {              
+                NavigateUri = new Uri("https://github.com/jpdominguez/NAVIS-Tool")
+            };
+            Hyperlink hyperlink2 = new Hyperlink(run3)
+            {
+                NavigateUri = new Uri("mailto:jpdominguez@atc.us.es?subject=NAVIS_Support&body=")
+            };
+            Hyperlink hyperlink3 = new Hyperlink(run5)
+            {
+                NavigateUri = new Uri("http://www.rtc.us.es")
+            };
+            Hyperlink hyperlink4 = new Hyperlink(run7)
+            {
+                NavigateUri = new Uri("http://www.atc.us.es")
+            };
+            Hyperlink hyperlink5 = new Hyperlink(run10)
+            {
+                NavigateUri = new Uri("http://www.informatica.us.es")
+            };
+
+            //Creating hyperlinks events
+            hyperlink1.RequestNavigate += new RequestNavigateEventHandler(delegate (object sender, RequestNavigateEventArgs e) {
+                Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+                e.Handled = true;
+            });
+            hyperlink2.RequestNavigate += new RequestNavigateEventHandler(delegate (object sender, RequestNavigateEventArgs e) {
+                Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+                e.Handled = true;
+            });
+            hyperlink3.RequestNavigate += new RequestNavigateEventHandler(delegate (object sender, RequestNavigateEventArgs e) {
+                Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+                e.Handled = true;
+            });
+            hyperlink4.RequestNavigate += new RequestNavigateEventHandler(delegate (object sender, RequestNavigateEventArgs e) {
+                Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+                e.Handled = true;
+            });
+            hyperlink5.RequestNavigate += new RequestNavigateEventHandler(delegate (object sender, RequestNavigateEventArgs e) {
+                Process.Start(new ProcessStartInfo(e.Uri.AbsoluteUri));
+                e.Handled = true;
+            });
+
+            //Filling the textblock with content
+            textInfo.Inlines.Clear();
+            textInfo.Inlines.Add(hyperlink1);
+            textInfo.Inlines.Add("\n");
+            textInfo.Inlines.Add(run2);
+            textInfo.Inlines.Add("\n");
+            textInfo.Inlines.Add(hyperlink2);
+            textInfo.Inlines.Add("\n\n");
+            textInfo.Inlines.Add(run4);
+            textInfo.Inlines.Add("\n");
+            textInfo.Inlines.Add(hyperlink3);
+            textInfo.Inlines.Add("\n\n");
+            textInfo.Inlines.Add(run6);
+            textInfo.Inlines.Add("\n");
+            textInfo.Inlines.Add(hyperlink4);
+            textInfo.Inlines.Add("\n\n");
+            textInfo.Inlines.Add(run8);
+            textInfo.Inlines.Add("\n");
+            textInfo.Inlines.Add(run9);
+            textInfo.Inlines.Add("\n");
+            textInfo.Inlines.Add(hyperlink5);
+            #endregion
+
+            //Show NAVIS version in a textblock
             versionInfo.Text = "v" + version.ToString();
         }
     }
